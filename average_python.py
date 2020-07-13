2febd34571fa494f396dc53fb84023893081203b
Miwa Ichihana <ichihana0517@icloud.com>
2020-07-13 11:42:43 +0900

    Add files via upload

---
+++ average_python.py
@@ -0,0 +1,11 @@
+import numpy as np
+import scipy as sp
+from func1 import func1
+
+x = np.array([1,2,3,4,5])
+x
+
+N = len(x)
+sum_value = sp.sum(x)
+mu = sum_value / N
+print(mu)
---
+++ func1.py
@@ -0,0 +1,6 @@
+import numpy as np
+import scipy as sp
+
+def func1(x):
+    s = sp.sum(x)
+    return s
